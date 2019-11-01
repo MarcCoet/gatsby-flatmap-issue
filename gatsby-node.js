740a24f33b38a472788c6827ed5e110b303990a8
MarcCoet <marcoet@gmail.com>
2019-11-01 14:34:13 +0100

    create schema and try to use it

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,16 @@
+const fs = require("fs")
+
 exports.createSchemaCustomization = ({ actions }) => {
-  actions.printTypeDefinitions({})
+  // by default saves to `schema.gql`
+  // const options = {
+  // path: `typedefs.graphql`,
+  // exclude: { types: [`TypeWeDontWant`] },
+  //   }
+  //
+  // Save the schema
+  // actions.printTypeDefinitions({
+  //   path: "schema.gql",
+  // })
+  // Use an existing schema
+  actions.createTypes(fs.readFileSync(`schema.gql`, { encoding: `utf-8` }))
 }
